e9cc5ee67218dae4fe8612c3ce9a3b6fe2bebb84
guifabre <guijoseph.fabre@gmail.com>
2023-01-23 19:05:09 +0000

    update doc harmonizRv3

---
+++ update_files.R
@@ -0,0 +1,11 @@
+library(harmonizR)
+library(fs)
+
+dir_copy(
+  "/staff/gfabre/harmonizRv3/docs",
+  "/staff/gfabre/harmonizR-documentation/docs",
+  overwrite = TRUE
+)
+
+# usethis::use_pkgdown()
+pkgdown::build_site()
