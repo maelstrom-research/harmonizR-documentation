07c1783a25531f38a4173079df9258bac55f8727
guifabre <guijoseph.fabre@gmail.com>
2023-01-24 12:31:02 +0000

    global update of documentation

--- update_files.R
+++ update_files.R
@@ -5,8 +5,11 @@ library(fs)
 devtools::document(roclets = c('rd', 'collate', 'namespace', 'vignette'))
 pkgdown::build_site()
 
-dir_copy(
+fs::dir_copy(
   "/staff/gfabre/harmonizRv3/docs",
   "/staff/gfabre/harmonizR-documentation/docs",
   overwrite = TRUE
 )
+
+
+harmonizRv3::harmonizR_help()
