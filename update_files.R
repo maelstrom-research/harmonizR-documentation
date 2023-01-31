70961911d1165a87fa5062470595d83116b027f1
a-trottier <atrottier@maelstrom-research.org>
2023-01-31 22:04:49 +0000

    add coming soon

--- update_files.R
+++ update_files.R
@@ -6,8 +6,8 @@ devtools::document(roclets = c('rd', 'collate', 'namespace', 'vignette'))
 pkgdown::build_site()
 
 fs::dir_copy(
-  "/staff/gfabre/harmonizRv3/docs",
-  "/staff/gfabre/harmonizR-documentation/docs",
+  "/staff/atrottier//harmonizRv3/docs",
+  "/staff/atrottier//harmonizR-documentation/docs",
   overwrite = TRUE
 )
 
