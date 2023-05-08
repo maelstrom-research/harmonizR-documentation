ab1d6e142659a355221cc707ffe56555b0c9b3c9
GuiFabre <guijoseph.fabre@gmail.com>
2023-05-08 09:11:52 -0400

    global update of documentation

--- update_files.R
+++ update_files.R
@@ -6,12 +6,10 @@ devtools::document(roclets = c('rd', 'collate', 'namespace', 'vignette'))
 pkgdown::build_site()
 
 fs::dir_copy(
-  "/staff/atrottier//harmonizR/docs",
-  "/staff/atrottier//harmonizR-documentation/docs",
+  "docs",
+  "../harmonizR-documentation/docs",
   overwrite = TRUE
 )
-
-
 
 
 harmonizRv3::harmonizR_help()
