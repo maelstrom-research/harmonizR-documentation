738665d75e0a8a2bd28cec5fda712a137310bf3b
GuiFabre <guijoseph.fabre@gmail.com>
2023-05-08 13:46:38 -0400

    update doc

--- update_files.R
+++ update_files.R
@@ -4,9 +4,11 @@ library(fabR)
 library(fs)
 # usethis::use_pkgdown()
 
+fs::dir_delete("docs")
 devtools::document(roclets = c('rd', 'collate', 'namespace', 'vignette'))
 pkgdown::build_site()
 
+fs::dir_delete("../harmonizR-documentation/docs")
 fs::dir_copy(
   "docs",
   "../harmonizR-documentation/docs",
